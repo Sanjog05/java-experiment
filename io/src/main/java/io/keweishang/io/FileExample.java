e6bb62ceb35b6383b5cb3df45ccbcb0d12da80b1
Kewei SHANG <kewei.shang@gmail.com>
2019-06-16 00:10:37 +0200

    add java io examples

---
+++ io/src/main/java/io/keweishang/io/FileExample.java
@@ -0,0 +1,27 @@
+package io.keweishang.io;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileExample {
+
+    public static void main(String[] args) throws IOException {
+        // absolute path
+        printFile("/foo/bar/my_absolute_file");
+
+        // relative path
+        printFile("my_relative_file");
+    }
+
+    private static void printFile(String path) throws IOException {
+        File file = new File(path);
+        System.out.println("Absolute path = " + file.getAbsolutePath());
+        System.out.println("Canonical path = " + file.getCanonicalPath());
+        System.out.println("Name = " + file.getName());
+        System.out.println("Parent = " + file.getParent());
+        System.out.println("Path = " + file.getPath());
+        System.out.println("Is absolute = " + file.isAbsolute());
+        System.out.println();
+    }
+
+}
