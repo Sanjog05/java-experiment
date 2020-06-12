a2720bf7f5a6fc333d8696cac57847c8fd20d869
Kewei Shang <kewei.shang@bolt.eu>
2020-06-12 13:50:56 +0300

    Add comment on mysql binlog reader

--- mysql-binlog-connector/src/main/java/io/keweishang/mysqlbinlogconnector/MySQLBinlogReaderApp.java
+++ mysql-binlog-connector/src/main/java/io/keweishang/mysqlbinlogconnector/MySQLBinlogReaderApp.java
@@ -17,13 +17,30 @@ public class MySQLBinlogReaderApp {
                 EventDeserializer.CompatibilityMode.CHAR_AND_BINARY_AS_BYTE_ARRAY
         );
         client.setEventDeserializer(eventDeserializer);
-        client.registerEventListener(new BinaryLogClient.EventListener() {
+        client.registerEventListener(event -> System.out.printf("%s\n", event));
+        /* Insert 1 row
+        Event{header=EventHeaderV4{timestamp=1591958619000, eventType=GTID, serverId=223344, headerLength=19, dataLength=46, nextPosition=582, flags=0}, data=GtidEventData{flags=0, gtid='294f815e-aa32-11ea-9580-0242ac110003:3'}}
+        Event{header=EventHeaderV4{timestamp=1591958619000, eventType=QUERY, serverId=223344, headerLength=19, dataLength=56, nextPosition=657, flags=8}, data=QueryEventData{threadId=2, executionTime=0, errorCode=0, database='test_db', sql='BEGIN'}}
+        Event{header=EventHeaderV4{timestamp=1591958619000, eventType=TABLE_MAP, serverId=223344, headerLength=19, dataLength=39, nextPosition=715, flags=0}, data=TableMapEventData{tableId=108, database='test_db', table='test_table', columnTypes=15, columnMetadata=765, columnNullability={0}}}
+        Event{header=EventHeaderV4{timestamp=1591958619000, eventType=EXT_WRITE_ROWS, serverId=223344, headerLength=19, dataLength=21, nextPosition=755, flags=0}, data=WriteRowsEventData{tableId=108, includedColumns={0}, rows=[
+            [[B@49167dab]
+        ]}}
+        Event{header=EventHeaderV4{timestamp=1591958619000, eventType=XID, serverId=223344, headerLength=19, dataLength=12, nextPosition=786, flags=0}, data=XidEventData{xid=13}}
+         */
 
-            @Override
-            public void onEvent(Event event) {
-                System.out.printf("%s\n", event);
-            }
-        });
+        /* Insert 2 rows in transaction
+        Event{header=EventHeaderV4{timestamp=1591958769000, eventType=GTID, serverId=223344, headerLength=19, dataLength=46, nextPosition=851, flags=0}, data=GtidEventData{flags=0, gtid='294f815e-aa32-11ea-9580-0242ac110003:4'}}
+        Event{header=EventHeaderV4{timestamp=1591958759000, eventType=QUERY, serverId=223344, headerLength=19, dataLength=56, nextPosition=926, flags=8}, data=QueryEventData{threadId=2, executionTime=0, errorCode=0, database='test_db', sql='BEGIN'}}
+        Event{header=EventHeaderV4{timestamp=1591958759000, eventType=TABLE_MAP, serverId=223344, headerLength=19, dataLength=39, nextPosition=984, flags=0}, data=TableMapEventData{tableId=108, database='test_db', table='test_table', columnTypes=15, columnMetadata=765, columnNullability={0}}}
+        Event{header=EventHeaderV4{timestamp=1591958759000, eventType=EXT_WRITE_ROWS, serverId=223344, headerLength=19, dataLength=21, nextPosition=1024, flags=0}, data=WriteRowsEventData{tableId=108, includedColumns={0}, rows=[
+            [[B@70269bc5]
+        ]}}
+        Event{header=EventHeaderV4{timestamp=1591958763000, eventType=TABLE_MAP, serverId=223344, headerLength=19, dataLength=39, nextPosition=1082, flags=0}, data=TableMapEventData{tableId=108, database='test_db', table='test_table', columnTypes=15, columnMetadata=765, columnNullability={0}}}
+        Event{header=EventHeaderV4{timestamp=1591958763000, eventType=EXT_WRITE_ROWS, serverId=223344, headerLength=19, dataLength=21, nextPosition=1122, flags=0}, data=WriteRowsEventData{tableId=108, includedColumns={0}, rows=[
+            [[B@a274b4f]
+        ]}}
+        Event{header=EventHeaderV4{timestamp=1591958769000, eventType=XID, serverId=223344, headerLength=19, dataLength=12, nextPosition=1153, flags=0}, data=XidEventData{xid=16}}
+         */
         client.connect(1000 * 5L);
     }
 }
